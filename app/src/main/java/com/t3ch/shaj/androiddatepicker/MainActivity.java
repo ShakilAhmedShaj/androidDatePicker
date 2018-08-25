735ab8d626a6da60dc0432a5c157c89faac1b4c9
SHAJ <shakilahmedshaj@gmail.com>
2018-08-25 16:42:25 +0600

    todays date added

---
+++ app/src/main/java/com/t3ch/shaj/androiddatepicker/MainActivity.java
@@ -0,0 +1,42 @@
+package com.t3ch.shaj.androiddatepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private DatePicker datePicker;
+    private Button selectButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.TextView_id);
+        datePicker = findViewById(R.id.DatePicker_id);
+        selectButton = findViewById(R.id.button_id);
+
+        textView.setText(currentDate());
+
+
+    }
+
+    String currentDate() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Current Date : ");
+        stringBuilder.append(datePicker.getDayOfMonth() + "/");
+        stringBuilder.append((datePicker.getMonth()+1) + "/");
+        stringBuilder.append(datePicker.getYear());
+
+        return stringBuilder.toString();
+
+    }
+
+
+}
